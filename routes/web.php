e7842966ec82ae772129c67ef1035b845df4f18a
popo0015 <popo0015@hz.nl>
2024-06-01 12:37:12 +0200

    Redirect dashboard to welcome page and ensure pages are accessible

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect('/');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
